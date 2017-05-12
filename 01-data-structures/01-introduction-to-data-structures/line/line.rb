4b9ef2a241bae22f0ffd50fedb46b16a21fae2f5
rlandows <rlandows@uci.edu>
2017-05-11 18:04:24 -0700

    line and screen questions

---
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -0,0 +1,69 @@
+# A line of people at an amusement park ride
+# There is a front to the line, as well as a back.
+# People may leave the line whenever they see fit and those behind them take their place.
+class Line
+  attr_accessor :members
+
+  def initialize
+    self.members = []
+  end
+
+  def join(person)
+    self.members << person
+
+  end
+
+  def leave(person)
+    self.members.delete(person)
+
+  end
+
+  def front
+    self.members[0]
+
+  end
+
+  def middle
+    length = self.members.length
+    middle = (length/2).floor
+    self.members[middle]
+
+  end
+
+  def back
+    self.members[-1]
+  end
+
+  def search(person)
+    if index(person).class == Fixnum
+      return person
+    else
+      return index(person)
+    end
+  end
+
+  private
+
+  def index(person)
+    self.members.each do |name, idx|
+      if person == name
+        return name
+      end
+    end
+    return nil
+  end
+
+end
+
+# hello = Line.new.join("harold")
+# Line.new.leave("jake")
+# Line.join("harold")
+
+# the_line = Line.new
+# the_line.join("kumar")
+# the_line.join("tricia")
+# the_line.leave("kevin")
+# the_line.front
+# the_line.middle
+# the_line.back
+# puts the_line.search("tfds")
---
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -0,0 +1,37 @@
+require_relative 'pixel'
+
+class Screen
+  attr_accessor :width
+  attr_accessor :height
+  attr_accessor :matrix
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+    @hash = Hash.new
+  end
+
+  # Insert a Pixel at x, y
+  def insert(pixel, x, y)
+    pixel.x = x
+    pixel.y = y
+
+    @hash[[x,y]] = pixel
+  end
+
+  def at(x, y)
+    @hash.each do |coordinates,value|
+      if [x,y] == coordinates
+        return value
+      end
+    end
+
+    return nil
+  end
+
+  private
+
+  def inbounds(x, y)
+  end
+
+end
